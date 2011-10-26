f736d5a12269e7e74740b130cdca98d9839b31e6
Stephen Hines <srhines@google.com>
2011-10-26 16:18:14 -0700

    Add back pre-ICS support for "int root(int);".
    BUG=5521638
    
    Change-Id: Ic1f3c071562c687a98125c2151e306313c5405b8

--- slang_rs_export_foreach.cpp
+++ slang_rs_export_foreach.cpp
@@ -292,7 +292,8 @@ RSExportForEach *RSExportForEach::Create(RSContext *Context,
   return FE;
 }
 
-bool RSExportForEach::isRSForEachFunc(const clang::FunctionDecl *FD) {
+bool RSExportForEach::isRSForEachFunc(int targetAPI,
+    const clang::FunctionDecl *FD) {
   // We currently support only compute root() being exported via forEach
   if (!isRootRSFunc(FD)) {
     return false;
@@ -302,10 +303,23 @@ bool RSExportForEach::isRSForEachFunc(const clang::FunctionDecl *FD) {
     // Graphics compute function
     return false;
   }
+
+  // Handle legacy graphics root functions.
+  if ((targetAPI < SLANG_ICS_TARGET_API) && (FD->getNumParams() == 1)) {
+    const clang::ParmVarDecl *PVD = FD->getParamDecl(0);
+    clang::QualType QT = PVD->getType().getCanonicalType();
+    const clang::QualType &IntType = FD->getASTContext().IntTy;
+    if ((FD->getResultType().getCanonicalType() == IntType) &&
+        (QT == IntType)) {
+      return false;
+    }
+  }
+
   return true;
 }
 
-bool RSExportForEach::validateSpecialFuncDecl(clang::Diagnostic *Diags,
+bool RSExportForEach::validateSpecialFuncDecl(int targetAPI,
+                                              clang::Diagnostic *Diags,
                                               const clang::FunctionDecl *FD) {
   slangAssert(Diags && FD);
   bool valid = true;
@@ -323,6 +337,9 @@ bool RSExportForEach::validateSpecialFuncDecl(clang::Diagnostic *Diags,
                                    "an int for graphics usage"));
         valid = false;
       }
+    } else if ((targetAPI < SLANG_ICS_TARGET_API) && (numParams == 1)) {
+      // Legacy graphics root function
+      // This has already been validated in isRSForEachFunc().
     } else {
       slangAssert(false &&
           "Should not call validateSpecialFuncDecl() on compute root()");
