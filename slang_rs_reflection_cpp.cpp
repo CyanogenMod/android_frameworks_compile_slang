f5ab7e790bb4a2d5d6495d37b0855005d48f91c3
Jean-Luc Brouillet <jeanluc@google.com>
2014-07-25 15:50:32 -0700

    Add comments to the generated file.
    These comments are help our users understand how to use the reflected code.
    I'll add these to the Java reflection too after we settle the details for
    the C++ version.
    
    Change-Id: I887dbd78c52218cafe907973829458340efbf388

--- slang_rs_reflection_cpp.cpp
+++ slang_rs_reflection_cpp.cpp
@@ -143,8 +143,11 @@ bool RSReflectionCpp::writeHeaderFile() {
   mOut.indent() << "#include \"RenderScript.h\"\n\n";
   mOut.indent() << "using namespace android::RSC;\n\n";
 
-  mOut.indent() << "class " << mClassName
-                << " : public android::RSC::ScriptC";
+  mOut.comment("This class encapsulates access to the exported elements of the script.  "
+               "Typically, you would instantiate this class once, call the set_* methods "
+               "for each of the exported global variables you want to change, then call "
+               "one of the forEach_ methods to invoke a kernel.");
+  mOut.indent() << "class " << mClassName << " : public android::RSC::ScriptC";
   mOut.startBlock();
 
   mOut.decreaseIndent();
@@ -195,27 +198,47 @@ void RSReflectionCpp::genTypeInstancesUsedInForEach() {
 }
 
 void RSReflectionCpp::genFieldsForAllocationTypeVerification() {
+  bool CommentAdded = false;
   for (std::set<std::string>::iterator I = mTypesToCheck.begin(),
                                        E = mTypesToCheck.end();
        I != E; I++) {
+    if (!CommentAdded) {
+      mOut.comment("The following elements are used to verify the types of "
+                   "allocations passed to kernels.");
+      CommentAdded = true;
+    }
     mOut.indent() << "android::RSC::sp<const android::RSC::Element> "
                   << RS_ELEM_PREFIX << *I << ";\n";
   }
 }
 
 void RSReflectionCpp::genFieldsToStoreExportVariableValues() {
+  bool CommentAdded = false;
   for (RSContext::const_export_var_iterator I = mRSContext->export_vars_begin(),
                                             E = mRSContext->export_vars_end();
        I != E; I++) {
     const RSExportVar *ev = *I;
-    if (!ev->isConst()) {
-      mOut.indent() << GetTypeName(ev->getType()) << " " RS_EXPORT_VAR_PREFIX
-                    << ev->getName() << ";\n";
+    if (ev->isConst()) {
+      continue;
     }
+    if (!CommentAdded) {
+      mOut.comment("For each non-const variable exported by the script, we "
+                   "have an equivalent field.  This field contains the last "
+                   "value this variable was set to using the set_ method.  "
+                   "This may not be current value of the variable in the "
+                   "script, as the script is free to modify its internal "
+                   "variable without changing this field.  If the script "
+                   "initializes the exported variable, the constructor will "
+                   "initialize this field to the same value.");
+      CommentAdded = true;
+    }
+    mOut.indent() << GetTypeName(ev->getType()) << " " RS_EXPORT_VAR_PREFIX
+                  << ev->getName() << ";\n";
   }
 }
 
 void RSReflectionCpp::genForEachDeclarations() {
+  bool CommentAdded = false;
   for (RSContext::const_export_foreach_iterator
            I = mRSContext->export_foreach_begin(),
            E = mRSContext->export_foreach_end();
@@ -225,6 +248,15 @@ void RSReflectionCpp::genForEachDeclarations() {
     if (ForEach->isDummyRoot()) {
       mOut.indent() << "// No forEach_root(...)\n";
       continue;
+    }
+
+    if (!CommentAdded) {
+      mOut.comment("For each kernel of the script corresponds one method.  "
+                   "That method queues the kernel for execution.  The kernel "
+                   "may not have completed nor even started by the time this "
+                   "function returns.  Calls that extract the data out of the "
+                   "output allocation will wait for the kernels to complete.");
+      CommentAdded = true;
     }
 
     std::string FunctionStart = "void forEach_" + ForEach->getName() + "(";
@@ -458,6 +490,13 @@ bool RSReflectionCpp::writeImplementationFile() {
 }
 
 void RSReflectionCpp::genExportVariablesGetterAndSetter() {
+  mOut.comment("Methods to set and get the variables exported by the script. "
+               "Const variables will not have a setter.\n\n"
+               "Note that the value returned by the getter may not be the "
+               "current value of the variable in the script.  The getter will "
+               "return the initial value of the variable (as defined in the "
+               "script) or the the last value set by using the setter method.  "
+               "The script is free to modify its value independently.");
   for (RSContext::const_export_var_iterator I = mRSContext->export_vars_begin(),
                                             E = mRSContext->export_vars_end();
        I != E; I++) {
