9764eb3be0f2dcaaefa62fddc9fbedf14784ae38
Jean-Luc Brouillet <jeanluc@google.com>
2015-08-07 16:43:18 -0700

    Change the name for the new rs_context parameter.
    Change the reserved name for the new rs_context parameter to be "context".
    
    Since there can be only one rs_context parameter, report an  error if the
    name is incorrect.
    
    Also use "special parameter" consistently in the error messages to help
    our developers distinguish between regular and special parameters.
    
    Finally, some refactoring to make the code more understandable.
    
    b/21647353
    
    Change-Id: I6f23dc5a8ba3659714ad1f7ba41836ff8b92d9ec

--- tests/F_ctxt_wrong_place/F_ctxt_wrong_place.rs
+++ tests/F_ctxt_wrong_place/F_ctxt_wrong_place.rs
@@ -2,10 +2,10 @@
 #pragma version(1)
 #pragma rs java_package_name(foo)
 
-int RS_KERNEL good(int in, rs_kernel_context ctxt, uint32_t x) {
+int RS_KERNEL good(int in, rs_kernel_context context, uint32_t x) {
   return 0;
 }
 
-int RS_KERNEL bad(int in, uint32_t x, rs_kernel_context ctxt) {
+int RS_KERNEL bad(int in, uint32_t x, rs_kernel_context context) {
   return 0;
 }
